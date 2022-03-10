072fe845fac623beb38e5d1b51f5d2cb1e3dcaa2
Lidia <maciellili@hotmail.com>
2022-03-10 10:06:04 -0300

    sidebar

---
+++ src/pages/orderResume/OrderResume.jsx
@@ -0,0 +1,12 @@
+import Sidebar from '../../components/Sidebar/Sidebar'
+import React from 'react'
+
+function OrderResume() {
+    return (
+        <>
+           <Sidebar></Sidebar>
+        </>
+    )
+}
+
+export default OrderResume
