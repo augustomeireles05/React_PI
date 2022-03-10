2b94179fdc44b521a53d06fc6a2e53a6d05b9306
LarissaBSouza <larissabandeirasouza@gmail.com>
2022-03-10 10:15:55 -0300

    Alterações Titulo 10/03/2022 10h15 Larissa

--- src/components/Tittle/MainTittle.jsx
+++ src/components/Tittle/MainTittle.jsx
@@ -3,9 +3,9 @@ import './MainTittle.css';
 function MainTittle(props) {
     return (
       <>
-        <div class="container titulo-pagina">
-          <div class="row">
-            <div class="col-12 justify-content-flex-start">
+        <div className="container titulo-pagina">
+          <div className="row">
+            <div className="col-12 justify-content-flex-start">
               <h1>{props.title}</h1>
             </div>
           </div>
