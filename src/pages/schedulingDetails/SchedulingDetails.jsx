e82201cba0a808629a7f57e5b6ee4f23d07a8c00
Guilherme <vianaguilherme29@gmail.com>
2022-03-11 01:02:48 -0300

    Pagina em andamento Paginas favoritos, pag feita resumo do agendamento  e ajuste em menu lateral pag meus dados

---
+++ src/pages/schedulingDetails/SchedulingDetails.jsx
@@ -0,0 +1,106 @@
+import './SchedulingDetails.css'
+import Header from '../../components/header/Header'
+import Footer from '../../components/footer/Footer'
+
+function schedulingDetails() {
+    return (
+        <>
+
+            <div className="container">
+
+                <div className="row-col-md-12 resumo-agendamento mt-5 mb-5">
+                    <h1 className="resumo ps-5 pb-2 pt-2">RESUMO DO AGENDAMENTO</h1>
+                </div>
+
+                <div className="row g-5 px-3">
+                    {/* inicio do menu lateral */}
+                    <div className="col-md-4 flex-row justify-content-center text-center">
+
+
+                        {/*Navbar*/}
+                        <nav className="navbar navbar-light border-bottom-menu titulo-menu mb-3 mt-0 pt-0 pb-2">
+                            <div className="container-fluid d-block text-center">
+                                <span>MENU</span>
+                            </div>
+                        </nav>
+
+                        <div className="btn-group-vertical background-container-subject me-3 shadow" role="group"
+                            aria-label="Vertical button group">
+                            <button type="button" className="btn button-menu-lateral sidebar-font border-button-sidebar-menu"><a
+                                className="link-solution" href="./meusdados.html">MEUS DADOS</a></button>
+                            <button type="button" className="btn button-menu-lateral sidebar-font border-button-sidebar-menu"><a
+                                className="link-solution" href="./pedidos.html">MEUS PEDIDOS</a></button>
+                            <button type="button" className="btn button-menu-lateral sidebar-font border-button-sidebar-menu"><a
+                                className="link-solution" href="./carrinho.html">CARRINHO DE COMPRA</a></button>
+                            <button type="button" className="btn button-menu-lateral sidebar-font border-button-sidebar-menu"><a
+                                className="link-solution" href="./listadesejos.html">MEUS FAVORITOS</a></button>
+                            <button type="button" className="btn button-menu-lateral sidebar-font border-button-sidebar-menu"><a
+                                className="link-solution" href="./home.html">SAIR</a></button>
+                        </div>
+
+
+                    </div>
+                    {/* fim do menu lateral */}
+
+
+                    {/* inicio da confirmação do pedido */}
+                    <div className="col-md-8">
+                        <div className="d-flex pedido-concluido justify-content-center flex-column mb-4">
+
+                            <span className="titulo-pricipal text-center pb-2 mb-3">AGENDAMENTO</span>
+
+
+                            {/* <img src="img/img-resumo-compra/certificado-de-garantia.png" className="img-certificado"
+                                alt="agendamento concluido"> */}
+
+                            <div className="sucesso d-flex justify-content-center align-items-center m-3">
+                                <h2 className="text-center titulo-sucesso-agendamento mb-0 me-1">AGENDAMENTO REALIZADO COM SUCESSO!
+                                </h2>
+                                {/* <img src="img/img-resumo-compra/checked.png" alt="sucesso" className="img-sucess"> */}
+                            </div>
+
+                            <p className="text-center numero-pedido">PROTOCOLO DE AGENDAMENTO: XXXXXXXXXX </p>
+                        </div>
+
+                        <div className="detalhes-pedido">
+                            <div className="detalhes-pedido d-flex justify-content-center align-items-center mb-3">
+                                <h2 className="titulo-detalhes-agendamento me-2 mb-0">DETALHES DO AGENDAMENTO</h2>
+                                <a href="#" />
+                                {/* <img src="img/img-resumo-compra/printer.png" alt="imprimir resumo do pedido"
+                                    className="print"</a> */}
+                            </div>
+
+                            <div className="row resumo-produto mb-3">
+                                <h3 className="col-12 mb-2 pt-2 subtitulo">PRODUTO</h3>
+                                <div className="produtos d-flex justify-content-around">
+                                    <div className="produto col-7">
+                                        <p className="nome-produto1">AUDI RS Q8 2021 AZUL</p>
+                                    </div>
+
+                                    <div className="qtd col-5">
+                                        <p className="qtd-produto1 text-end">QTD: 01</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="row data-agendamento mb-3">
+                                <h3 className="subtitulo col-6 pt-2">DATA DO AGENDAMENTO</h3>
+                                <p className="col-6 text-end pt-2">DD/mm/yyyy</p>
+                            </div>
+
+                            <div className="row valor-total mb-5">
+                                <h3 className="subtitulo col-6 pt-2">VALOR DO BEM</h3>
+                                <p className="col-6 text-end pt-2">R$400.150,00</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* fim da confirmação do pedido */}
+
+        </>
+    )
+}
+
+export default schedulingDetails
+
---
+++ src/pages/wishList/WishList.jsx
@@ -0,0 +1,232 @@
+import './WishList.css'
+import Header from '../../components/header/Header'
+import Footer from '../../components/footer/Footer'
+import { Link } from 'react-router-dom'
+
+function WishList() {
+    return (
+        <>
+
+
+            <section>
+
+            <article class="page-content">   
+
+               
+                    {/* coluna Menu */}
+                    <div className="row responsive-mobile">
+                        <div className="col-3 d-md-block d-none">
+                            {/*responsividade menu */}
+                            <div className="column ms-4">
+
+
+                                {/*Navbar*/}
+                                <nav className="navbar navbar-light border-bottom-menu">
+                                    <div className="container-fluid d-block text-center">
+                                        <span className=" mb-0 ">MENU</span>
+
+
+
+                                    </div>
+                                </nav>
+
+                                <div className="btn-group-vertical background-container-subject shadow" role="group"
+                                    aria-label="Vertical button group">
+                                    <button type="button" className="btn button-menu-lateral sidebar-font border-button-sidebar-menu"><Link
+                                        className="link-solution" to="/MyData"> MEUS DADOS</Link></button>
+                                    <button type="button" className="btn button-menu-lateral sidebar-font border-button-sidebar-menu"><Link
+                                        className="link-solution" to="/myOrder"> MEUS PEDIDOS</Link></button>
+                                    <button type="button" className="btn button-menu-lateral sidebar-font border-button-sidebar-menu"><Link
+                                        className="link-solution" to="/cart"> CARRINHO DE COMPRA</Link></button>
+                                    {/* <button type="button" className="btn button-menu-lateral sidebar-font border-button-sidebar-menu"><Link
+                                        className="link-solution" to="/WishList> MEUS FAVORITOS</Link></button> */}
+                                    <button type="button" className="btn button-menu-lateral sidebar-font border-button-sidebar-menu"><Link
+                                        className="link-solution" to="/checkoutOrder"> CHECKOUT</Link ></button>
+                                    <button type="button" className="btn button-menu-lateral sidebar-font border-button-sidebar-menu"><Link
+                                        className="link-solution" to="/"> SAIR </Link></button>
+                                </div>
+
+                            </div>
+                        </div>
+                        {/* coluna lista desejos*/}
+                        <div className="col-9">
+                            <div className="">
+                                <div className="">
+                                    <div className=" text-center title col-12 ">
+                                        <span>LISTA DE DESEJOS</span>
+                                    </div>
+                                </div>
+
+
+
+
+                                <div className="">
+                                    <div className="listadesejos">
+
+                                        <div className="row row-cols-1 row-cols-md-3 g-4 responsive-ipadPro">
+
+                                            {/* card 1 */}
+                                            <div className="col text-center">
+                                                <Link to="#" className="text-decoration-none" />
+                                                <div className="card h-100 mb-5">
+                                                    <img src="../img/Chrysler_pacifica_Hybrida_2019_prata_01-768x576.jpg"
+                                                        className="card-img" alt="..."/>
+                                                        <div className="card-body">
+                                                            <h5 className="card-title">CHRYSLER PACIFICA XS 2019 </h5>
+                                                            <p className="card-text">R$579.000,00</p>
+                                                            <Link className="btn btn-warning mt-3 buttom-width" to="#" role="button" />
+                                                            COMPRAR <Link />
+                                                            <Link to="#"/><img src="./img/img-header/favorite.png" alt="favotirar"
+                                                                className="favoritar"/><Link />
+                                                            </div>
+                                                        </div>
+
+                                                        <Link />
+                                                </div>
+                                                {/* fim do card 1 */}
+
+
+
+                                                {/* card 2 */}
+                                                <div className="col text-center">
+                                                    <Link to="#" className="text-decoration-none" />
+                                                    <div className="card h-100 mb-5">
+                                                        <img src="../img/Jaguar_EPace_P250_STD_2018_branco_01-768x576.jpg"
+                                                            className="card-img" alt="..."/>
+                                                            <div className="card-body">
+                                                                <h5 className="card-title">JAGUAR E-PACE P-250 2018</h5>
+                                                                <p className="card-text">R$209.000,00</p>
+                                                                <Link className="btn btn-warning mt-3 buttom-width" to="#" role="button" />
+                                                                COMPRAR <Link />
+                                                                {/* <Link to="#" /><img src="./img/img-header/favorite.png" alt="favotirar" />
+                                                                className="favoritar"<Link /> */}
+                                                            </div>
+                                                    </div>
+                                                    <Link />
+                                                </div>
+                                                {/* fim do card 2 */}
+
+
+
+                                                {/* card 3 */}
+                                                <div className="col text-center">
+                                                    <Link to="#" className="text-decoration-none" />
+                                                    <div className="card h-100 mb-5">
+                                                        <img src="../img/Land_Rover_Evoque_HSE_Dynamic_Preta_2020_01-768x576.jpg"
+                                                            className="card-img" alt="..."/>
+                                                            <div className="card-body">
+                                                                <h5 className="card-title">LAND ROVER EVOQUE 2020</h5>
+                                                                <p className="card-text">R$399.000,00</p>
+                                                                <Link className="btn btn-warning mt-3 buttom-width" to="#" role="button" />
+                                                                COMPRAR <Link />
+                                                                {/* <Link to="#" /><img src="./img/img-header/favorite.png" alt="favotirar"
+                                                                    className="favoritar"/><Link />
+                                                            </div> */}
+                                                    </div>
+                                                    <Link />
+                                                </div>
+                                                {/* fim do card 3 */}
+
+                                                {/* card 4 */}
+                                                <div className="col text-center">
+                                                    <Link to="#" className="text-decoration-none" />
+                                                    <div className="card h-100 mb-5">
+                                                        {/* <img src="../img/Mustang_GT_Cinza_2019_01-184x130.jpg" className="card-img"
+                                                            alt="..."/> */}
+                                                            <div className="card-body">
+                                                                <h5 className="card-title">FORD MUSTANG GT 2020</h5>
+                                                                <p className="card-text">R$425.000,00</p>
+                                                                <Link className="btn btn-warning mt-3 buttom-width" to="#" role="button" />
+                                                                COMPRAR <Link />
+                                                                {/* <Link to="#" /><img src="./img/img-header/favorite.png" alt="favotirar"
+                                                                    className="favoritar"/><Link /> */}
+                                                            </div>
+                                                    </div>
+                                                    <Link />
+                                                </div>
+                                                {/* fim do card 4 */}
+
+
+                                                {/* card 5 */}
+                                                <div className="col text-center">
+                                                    <Link to="#" className="text-decoration-none" />
+                                                    <div className="card h-100 mb-5">
+                                                        <img src="../img/Porsche_911_Carrera_2021_giz_01-184x130.jpg"
+                                                            className="card-img" alt="..."/>
+                                                            <div className="card-body">
+                                                                <h5 className="card-title">PORSCHE 911 CARRERA 2021</h5>
+                                                                <p className="card-text">R$1.030.000,00</p>
+                                                                <Link className="btn btn-warning mt-3 buttom-width" to="#" role="button" />
+                                                                COMPRAR <Link />
+                                                                {/* <Link to="#" /><img src="./img/img-header/favorite.png" alt="favotirar"
+                                                                    className="favoritar"/><Link /> */}
+                                                            </div>
+                                                    </div>
+                                                    <Link />
+                                                </div>
+                                                {/* fim do card 5 */}
+
+
+                                                {/* card 6 */}
+                                                <div className="col text-center">
+                                                    <Link to="#" className="text-decoration-none" />
+                                                    <div className="card h-100 mb-5">
+                                                        {/* <img src="../img/Audi_A5_Sportback_Sline_2019_cinza_01-768x576.jpg"
+                                                            className="card-img" alt="..."/> */}
+                                                            <div className="card-body">
+                                                                <h5 className="card-title">AUDI A5 SPORTBACK 2019</h5>
+                                                                <p className="card-text">R$249.000,00</p>
+                                                                <Link className="btn btn-warning mt-3 buttom-width" to="#" role="button" />
+                                                                COMPRAR<Link />
+                                                                {/* <Link to="#" /><img src="./img/img-header/favorite.png" alt="favotirar"
+                                                                    className="favoritar"/><Link /> */}
+                                                            </div>
+                                                    </div>
+                                                    <Link />
+                                                </div>
+                                                {/* fim do card 6 */}
+
+
+                                                {/* card 7 */}
+                                                <div className="col mb-3 text-center">
+                                                    <Link to="#" className="text-decoration-none" />
+                                                    <div className="card h-100 mb-5">
+                                                        {/* <img src="../img/MercedesBenz_AMG_GT_63_S_2020_branco_01-768x576.jpg"
+                                                            className="card-img" alt="..."/> */}
+                                                            <div className="card-body">
+                                                                <h5 className="card-title">MERCEDES-BENZ AMG GLE 63 2018</h5>
+                                                                <p className="card-text">R$659.000,00</p>
+                                                                <Link className="btn btn-warning mt-3 buttom-width" to="#" role="button" />
+                                                                COMPRAR <Link />
+                                                                {/* <Link to="#" /><img src="./img/img-header/favorite.png" alt="favotirar"
+                                                                    className="favoritar"/><Link /> */}
+                                                            </div>
+                                                    </div>
+                                                    <Link />
+                                                </div>
+                                                {/* fim do card 7 */}
+
+
+
+                                            </div>
+
+
+                                        </div>
+
+                                    </div>
+                                </div>
+
+
+                            </div>
+                        </div>
+                    </div>
+                </article>
+
+            </section>
+
+
+       </>
+    )
+}
+
+export default WishList
