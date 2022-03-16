0c49a624a5e541013b51d6076eff6626b8708aa8
LarissaBSouza <larissabandeirasouza@gmail.com>
2022-03-15 22:08:26 -0300

    Adicionando Componente Button 15/03/2022 22h08 Larissa

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,11 +3,11 @@ import "./Button.css";
 function Button(props) {
   return (
     <>
-      <div className="row d-flex justify-content-center ">
+      <div className="d-flex justify-content-center">
         {/* <button type={props.type} className="btn btn-principal w-25">
           {props.name}
         </button> */}
-        <button className="btn d-block w-25 btn-principal" type="submit"><a href={props.link} className="link-referencia">{props.name}</a></button>
+        <button className="btn btn-principal" type="submit"><a href={props.link} className="link-referencia">{props.name}</a></button>
       </div>
     </>
   );
