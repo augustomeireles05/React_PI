dd54e0aab0c191db882f18cb50ddf28593688b56
Guilherme <vianaguilherme29@gmail.com>
2022-03-18 00:04:44 -0300

    ajustes dos botoes e pag que estavam pendentes

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,8 @@ import "./Button.css";
 function Button(props) {
   return (
     <>
-      <div className="d-flex justify-content-center ">
+      {/* Estava justify-content-center */}
+      <div className="d-flex justify-content-end me-3 me-md-0 "> 
         {/* <button type={props.type} className="btn btn-principal w-25">
           {props.name}
         </button> */}
