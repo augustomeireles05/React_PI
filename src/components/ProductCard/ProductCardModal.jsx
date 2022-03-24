d7a601039d36c8c9c4726a650f12ecb58d182bdd
Augusto Meireles <augustomeireles05@gmail.com>
2022-03-24 15:56:40 -0300

    Aplicação completa do Estilo do modal na component ProductCardModal

--- src/components/ProductCard/ProductCardModal.jsx
+++ src/components/ProductCard/ProductCardModal.jsx
@@ -44,40 +44,40 @@ export default class ProductCardModal extends Component {
                                 </div>
 
                                 <div className="col-12 col-sm-12 col-md-12 col-lg-6">
-                                    <div className="background-datasheet border border-2  border-dark">
+                                    <div className=" border border-2  border-dark background-datasheet">
                                         <h4 className="text-center my-2">Ficha Técnica</h4>
                                     </div>
 
                                     <div className="mt-2 border border-2  border-dark p-3 background-datasheet">
-                                        <div className="row justify-content-center col-12 border border-2  border-dark mx-0 d-flex align-items-center">
+                                        <div className="row justify-content-center col-12 border border-2  border-dark mx-0 d-flex align-items-center background-datasheet-lighter">
                                             <p className="col-4 text-start fs-6 my-0">Modelo:</p>
                                             <p className="col-8 css-font-family-modal text-end fs-6 my-0">{this.props.model}</p>
                                         </div>
-                                        <div className="row justify-content-center col-12 border border-2  border-dark mx-0 d-flex align-items-center mt-2">
+                                        <div className="row justify-content-center col-12 border border-2  border-dark mx-0 d-flex align-items-center mt-2 background-datasheet-lighter">
                                             <p className="col-6 text-start fs-6 my-0">Cor:</p>
                                             <p className=" col-6 css-font-family-modal text-end fs-6 my-0 css-font-family-modal">{this.props.color}</p>
                                         </div>
-                                        <div className="row justify-content-center col-12 border border-2  border-dark mx-0 d-flex align-items-center mt-2">
+                                        <div className="row justify-content-center col-12 border border-2  border-dark mx-0 d-flex align-items-center mt-2 background-datasheet-lighter">
                                             <p className="col-9 text-start fs-6 my-0">Ano de Fabricação:</p>
                                             <p className="col-3 css-font-family-modal text-end fs-6 my-0 css-font-family-modal">{this.props.year}</p>
                                         </div>
-                                        <div className="row justify-content-center col-12 border border-2  border-dark mx-0 d-flex align-items-center mt-2">
+                                        <div className="row justify-content-center col-12 border border-2  border-dark mx-0 d-flex align-items-center mt-2 background-datasheet-lighter">
                                             <p className="col-3 text-start fs-6 my-0">Motor:</p>
                                             <p className="col-9 css-font-family-modal text-end fs-6 my-0 css-font-family-modal">{this.props.engine}</p>
                                         </div>
-                                        <div className="row justify-content-center col-12 border border-2  border-dark mx-0 d-flex align-items-center mt-2">
+                                        <div className="row justify-content-center col-12 border border-2  border-dark mx-0 d-flex align-items-center mt-2 background-datasheet-lighter">
                                             <p className="col-6 text-start fs-6 my-0">Potência:</p>
                                             <p className="col-6 css-font-family-modal text-end fs-6 my-0 css-font-family-modal">{this.props.potency}</p>
                                         </div>
-                                        <div className="row justify-content-center col-12 border border-2  border-dark mx-0 d-flex align-items-center mt-2">
+                                        <div className="row justify-content-center col-12 border border-2  border-dark mx-0 d-flex align-items-center mt-2 background-datasheet-lighter">
                                             <p className="col-3 text-start fs-6 my-0">Câmbio:</p>
                                             <p className="col-9 css-font-family-modal text-end fs-6 my-0 css-font-family-modal">{this.props.gearshift}</p>
                                         </div>
-                                        <div className="row justify-content-center col-12 border border-2  border-dark mx-0 d-flex align-items-center mt-2">
+                                        <div className="row justify-content-center col-12 border border-2  border-dark mx-0 d-flex align-items-center mt-2 background-datasheet-lighter">
                                             <p className="col-6 text-start fs-6 my-0">Combustível:</p>
                                             <p className="col-6 css-font-family-modal text-end fs-6 my-0 css-font-family-modal">{this.props.fuel}</p>
                                         </div>
-                                        <div className="row justify-content-center col-12 border border-2  border-dark mx-0 d-flex align-items-center mt-2">
+                                        <div className="row justify-content-center col-12 border border-2  border-dark mx-0 d-flex align-items-center mt-2 background-datasheet-lighter">
                                             <p className="col-6 text-start fs-6 my-0">Preço:</p>
 
                                             {/* INICIO: APLICAÇÃO SEM O FORMATO DE MOEDA BRASILEIRA */}
@@ -103,7 +103,7 @@ export default class ProductCardModal extends Component {
                                 </div>
 
                                 <div className="col-12 col-sm-12 col-md-12 col-lg-12 justify-content-center">
-                                    <div className="background-datasheet border border-2  border-dark mt-2">
+                                    <div className=" border border-2  border-dark mt-2 background-datasheet">
                                         <h4 className="text-center my-2">Descrição do veículo</h4>
                                     </div>
 
